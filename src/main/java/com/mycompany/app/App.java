b35114f7477dcc944b6e385885d993290a1d4a01
PC <eeksi@etu.edu.tr>
2024-03-12 21:38:56 +0300

    trying to fix proble with jar file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -22,23 +22,14 @@ public class App {
         if (arr1 == null || arr2 == null) {
             return 0; // Return 0 for null arrays
         }
-
-        int count = 0;
-        Map<Integer, Integer> elementCount = new HashMap<>(); // Track element counts in arr1
-
-        // Build a map to store the count of each element in arr1
-        for (int num : arr1) {
-            elementCount.put(num, elementCount.getOrDefault(num, 0) + 1);
+        int localSum = 0;
+        for (Integer integer : arr2) {
+            localSum+=integer;
         }
+        localSum+=target;
 
-        for (int num : arr1) {
-            if (elementCount.get(num) > 0 && arr2.contains(num) && num == target) {
-                count++;
-                elementCount.put(num, elementCount.get(num) - 1); // Decrement element count in arr1
-            }
-        }
 
-        return count;
+        return localSum;
     }
 
     public static void main(String[] args) {
